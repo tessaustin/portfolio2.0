18218c21965d970716a7211d0dcd3de62216394d
Tess Austin <tessaustin92@gmail.com>
2021-06-07 20:22:55 -0600

    added proficiencies to resume section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,10 +3,37 @@ import React from "react";
 function Resume() {
     return (
         <section>
-            <div className="bottom-spacing">
+            <div className="file">
                 <a href={require("../../assets/file/resume.pdf").default} download>
-                    <h4>My Resume</h4>
+                    <h1>My Resume</h1>
                 </a>
+            </div>
+            <div>
+                <h3>Proficiencies</h3>
+                <ol>
+                    <li>HTML/CSS/Git</li>
+                    <li>JavaScript</li>
+                    <li>Bootstrap</li>
+                    <li>The DOM</li>
+                    <li>APIs</li>
+                    <li>JQuery</li>
+                    <li>JSON</li>
+                    <li>AJAX</li>
+                    <li>Node</li>
+                    <li>ES6</li>
+                    <li>Object-Oriented Programming</li>
+                    <li>Express</li>
+                    <li>MySQL</li>
+                    <li>MVC Paradigm</li>
+                    <li>Sequelize</li>
+                    <li>Testing</li>
+                    <li>Agile Development</li>
+                    <li>Progressive Web Apps</li>
+                    <li>React</li>
+                    <li>NoSQL</li>
+                    <li>MERN Stack</li>
+                    <li>Computer Science Fundamentals </li>
+                </ol>
             </div>
         </section>
     );
