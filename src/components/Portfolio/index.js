4fd690e22293146485334d2908bd99fb145a1236
Tess Austin <tessaustin92@gmail.com>
2021-06-07 21:40:11 -0600

    portfolio section displaying 6 projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,8 +5,27 @@ function Portfolio() {
     return (
         <div>
             <img
+                src={require("../../assets/img/petadoption.jpg").default}
+                alt="pet-adoption"
+                className="screenshot"
+            ></img>
+            <a            
+            href="https://avamariee.github.io/pet-adoption-quiz/"
+                target="_blank"
+                rel="noopener noreferrer">
+                <h5>Pet Adoption App</h5>
+            </a>
+            <a
+                href="https://github.com/avamariee/pet-adoption-quiz"
+                target="_blank"
+                rel="noopener noreferrer">
+                <h5>Pet Adoption Repository</h5>
+            </a>
+            <br></br>
+
+            <img
                 src={require("../../assets/img/notetaker.png").default}
-                alt="notetaker"
+                alt="note-taker"
                 className="screenshot"
             ></img>
             <a            
@@ -21,7 +40,103 @@ function Portfolio() {
                 rel="noopener noreferrer">
                 <h5>Note Taker Repository</h5>
             </a>
+            <br></br>
+
+            <img
+                src={require("../../assets/img/scheduler.png").default}
+                alt="scheduler"
+                className="screenshot"
+            ></img>
+            <a            
+            href="https://tessaustin.github.io/scheduler/"
+                target="_blank"
+                rel="noopener noreferrer">
+                <h5>Scheduler App</h5>
+            </a>
+            <a
+                href="https://github.com/tessaustin/scheduler"
+                target="_blank"
+                rel="noopener noreferrer">
+                <h5>Scheduler Repository</h5>
+            </a>
+            <br></br>
+
+            <img
+                src={require("../../assets/img/forecast.png").default}
+                alt="forecast"
+                className="screenshot"
+            ></img>
+            <a            
+            href="https://tessaustin.github.io/forecast/"
+                target="_blank"
+                rel="noopener noreferrer">
+                <h5>Forecast App</h5>
+            </a>
+            <a
+                href="https://github.com/tessaustin/forecast"
+                target="_blank"
+                rel="noopener noreferrer">
+                <h5>Forecast Repository</h5>
+            </a>
+            <br></br>
+
+            <img
+                src={require("../../assets/img/techblog.png").default}
+                alt="tech-blog"
+                className="screenshot"
+            ></img>
+            <a            
+            href="https://calm-fjord-73302.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer">
+                <h5>Tech Blog App</h5>
+            </a>
+            <a
+                href="https://github.com/tessaustin/MVC-tech"
+                target="_blank"
+                rel="noopener noreferrer">
+                <h5>Tech Blog Repository</h5>
+            </a>
+            <br></br>
+
+            <img
+                src={require("../../assets/img/password.png").default}
+                alt="password"
+                className="screenshot"
+            ></img>
+            <a            
+            href="https://tessaustin.github.io/password-generator/"
+                target="_blank"
+                rel="noopener noreferrer">
+                <h5>Password Generator App</h5>
+            </a>
+            <a
+                href="https://github.com/tessaustin/password-generator"
+                target="_blank"
+                rel="noopener noreferrer">
+                <h5>Password Generator Repository</h5>
+            </a>
+            <br></br>
+
+            <img
+                src={require("../../assets/img/budget.png").default}
+                alt="budget"
+                className="screenshot"
+            ></img>
+            <a            
+            href="https://polar-wildwood-09716.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer">
+                <h5>Budget App</h5>
+            </a>
+            <a
+                href="https://github.com/tessaustin/budget-real-good"
+                target="_blank"
+                rel="noopener noreferrer">
+                <h5>Budget Repository</h5>
+            </a>
         </div>
+        
     )
 
 };
